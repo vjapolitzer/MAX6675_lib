eddde22af4300f291916ef768b207140e4734062
Vincent Politzer <vjapolitzer@berkeley.edu>
2019-02-26 16:51:02 -0800

    Update style and add comments

--- MAX6675_lib.cpp
+++ MAX6675_lib.cpp
@@ -2,32 +2,38 @@
 #include "MAX6675_lib.h"
 #include "SPI.h"
 
-MAX6675::MAX6675(int8_t CS ) {
+MAX6675::MAX6675(int8_t CS)
+{
     this->cs = CS;
     pinMode(this->cs, OUTPUT);
     digitalWrite(this->cs, HIGH);
 }
 
-double MAX6675::readCelsius(void) {
-    uint16_t v;
-    //SPI clock speed:4.3MHz, Data Shift:MSB First, Data Clock Idle: SPI_MODE0
+double MAX6675::readCelsius()
+{
+    uint16_t data;
+    // SPI clock speed:4.3MHz, Data Shift:MSB First, Data Clock Idle: SPI_MODE0
     SPI.beginTransaction(SPISettings(4300000, MSBFIRST, SPI_MODE0));
+    // Select the chip, stopping the conversion process
     digitalWrite(this->cs, LOW);
-
-    v = SPI.transfer(0x00);
-    v <<= 8;
-    v |= SPI.transfer(0x00);
-
+    // Read in bits 15-8
+    data = SPI.transfer(0x00);
+    data <<= 8;
+    // Read in bits 7-0
+    data |= SPI.transfer(0x00);
+    // Deselect the chip, intiating a new conversion process
     digitalWrite(this->cs, HIGH);
     SPI.endTransaction();
 
-    if (v & 0x4) return NAN; // Thermocouple disconnected!
-
-    v >>= 3;
-
-    return v * 0.25;
+    // Check bit 2, which indicates whether the thermocouple is disconnected
+    if (data & 0x4) return NAN;
+    // Get rid of bits 2-0 (State, Device ID, Thermocouple Input)
+    data >>= 3;
+    // Scale the data, which is ~4x the actual value (4096 -> 1023.75C)
+    return data * 0.25;
 }
 
-double MAX6675::readFahrenheit(void) {
-    return readCelsius() * (9.0 / 5.0) + 32.0;
+double MAX6675::readFahrenheit()
+{
+    return (readCelsius() * 1.8) + 32.0;
 }
